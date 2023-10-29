8a019c3f97ac83adaaacdd605c46bf86f6860fa6
Emmanuel Briot <briot.emmanuel@gmail.com>
2023-10-29 21:38:31 +0100

    (FileFind): new class
    Finding the GPR files is now done via an iterator, no need to create
     an intermediate vector.

---
+++ src/findfile.rs
@@ -0,0 +1,72 @@
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
+
+pub struct FileFind {
+    iter: Vec<std::fs::ReadDir>,
+}
+
+impl FileFind {
+    /// Start searching for file in path, recursively
+    pub fn new(path: &Path) -> FileFind {
+        let mut f = FileFind { iter: vec![] };
+        f.pushdir(path);
+        f
+    }
+
+    /// Push a new directory to traverse (we will first return the entries from
+    /// that directory, then the remaining ones from the parent directory,
+    /// and so on).
+    fn pushdir(&mut self, path: &Path) {
+        match std::fs::read_dir(path) {
+            Ok(iter) => self.iter.push(iter),
+            Err(err) => {
+                println!("Error reading directory {}: {}", path.display(), err);
+            }
+        }
+    }
+
+    /// Whether the directory should be traversed
+    fn traverse_dir(&mut self, path: &Path) -> bool {
+        let meta = std::fs::symlink_metadata(path);
+        let n = path.as_os_str().to_str();
+        match (meta, n) {
+            (Ok(meta), Some(n)) =>
+                meta.is_dir()
+                && !meta.is_symlink()
+                && !n.ends_with("External/Ada_Web_Server/aws-dev")
+                && !n.ends_with("External/GNATCOLL/gnatcoll-dev")
+                && !n.ends_with("Packaging")
+                && !n.ends_with("Compiler")
+                && !n.ends_with(".dbc"),
+            _ => false
+        }
+    }
+}
+
+impl Iterator for FileFind {
+    type Item = PathBuf;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.iter.last_mut()?.next() {
+                None => {
+                    self.iter.pop(); //  Nothing else to read in child directory
+                }
+                Some(Ok(e)) => {
+                    let path = e.path();
+                    match path.extension().and_then(OsStr::to_str) {
+                        Some("gpr") => return Some(std::fs::canonicalize(path).unwrap()),
+                        _ => {
+                            if self.traverse_dir(&path) {
+                                self.pushdir(&path);
+                            }
+                        }
+                    }
+                }
+                Some(Err(err)) => {
+                    println!("Error {}", err);
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,39 @@
-use std::ffi::OsStr;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 pub mod environment;
 pub mod errors;
 pub mod expressions;
 pub mod files;
+pub mod findfile;
 pub mod gpr;
 pub mod lexer;
 pub mod scanner;
 pub mod tokens;
 
 use crate::environment::{Environment, GPRIndex};
-use crate::gpr::GPR;
 
-pub fn find_gpr_files(path: &Path, list_of_files: &mut Vec<PathBuf>) {
-    if let Ok(iter) = std::fs::read_dir(path) {
-        for e in iter {
-            match e {
-                Ok(e) => {
-                    let path = e.path();
-                    match path.extension().and_then(OsStr::to_str) {
-                        Some("gpr") => list_of_files.push(std::fs::canonicalize(path).unwrap()),
-                        _ => {
-                            if let Ok(meta) = std::fs::symlink_metadata(&path) {
-                                let name = path.as_os_str().to_str();
-                                if let Some(n) = name {
-                                    if !n.ends_with("External/Ada_Web_Server/aws-dev")
-                                        && !n.ends_with("External/GNATCOLL/gnatcoll-dev")
-                                        && !n.ends_with("Packaging")
-                                        && !n.ends_with("Compiler")
-                                        && !n.ends_with(".dbc")
-                                        && meta.is_dir()
-                                        && !meta.is_symlink()
-                                    {
-                                        find_gpr_files(&path, list_of_files);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                Err(err) => {
-                    println!("Error {}", err);
-                }
-            }
-        }
-    }
-}
-
-pub fn parse_gpr_file(path: &Path, env: &Environment) -> Result<GPR, Box<dyn std::error::Error>> {
-    let file = files::File::new(path)?;
-    let mut lex = lexer::Lexer::new(&file);
-    let scan = scanner::Scanner::new(&mut lex);
-    let gpr = scan.parse(env)?;
-    println!("{}", gpr);
-    Ok(gpr)
-}
-
-pub fn parse_all(list_of_gpr: &Vec<PathBuf>) -> Result<(), Box<dyn std::error::Error>> {
+pub fn parse_all(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let mut env = Environment::default();
 
     // Prepare the indexes for the GPR files, so that we can later have the list of dependencies
 
-    for gpr in list_of_gpr {
+    for gpr in crate::findfile::FileFind::new(path) {
         env.map.insert(gpr.to_path_buf(), env.gprs.len());
         env.gprs.push(None);
     }
 
-    for gpr in list_of_gpr {
-        let g = parse_gpr_file(gpr, &env)?;
-        let idx: GPRIndex = env.map[g.path()];
-        env.gprs[idx] = Some(g);
+    // Then parse the GPR files
+
+    for gpr in env.map.keys() {
+        let file = files::File::new(gpr)?;
+        let mut lex = lexer::Lexer::new(&file);
+        let scan = scanner::Scanner::new(&mut lex);
+        let gprfile = scan.parse(&env)?;
+        let idx: GPRIndex = env.map[gprfile.path()];
+        env.gprs[idx] = Some(gprfile);
     }
+
+    println!("Parsed {} gpr files", env.map.len());
 
     //    let pool = threadpool::ThreadPool::new(1);
     //    for gpr in list_of_gpr {
@@ -85,9 +47,7 @@ pub fn parse_all(list_of_gpr: &Vec<PathBuf>) -> Result<(), Box<dyn std::error::E
 }
 
 fn main() {
-    let mut list_of_gpr: Vec<PathBuf> = vec![];
-    find_gpr_files(Path::new("/home/briot/dbc/deepblue"), &mut list_of_gpr);
-    if let Err(e) = parse_all(&list_of_gpr) {
+    if let Err(e) = parse_all(Path::new("/home/briot/dbc/deepblue")) {
         println!("ERROR: {}", e);
     }
 }
