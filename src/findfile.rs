18e7838df1eace74adcfd6f08d0e99576b9b16da
Emmanuel Briot <briot.emmanuel@gmail.com>
2023-11-29 23:11:57 +0100

    (findfile.rs): rewritten to limit system calls
    We are now down to 26932 system calls

--- src/findfile.rs
+++ src/findfile.rs
@@ -1,14 +1,87 @@
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
+use std::fs::FileType;
+
+struct DirEntry {
+    path: PathBuf,
+
+    // Will always be needed, so save it.
+    ft: FileType,
+}
+
+impl DirEntry {
+
+    #[cfg(unix)]
+    pub(crate) fn from_entry(ent: &std::fs::DirEntry) -> Result<Self, String> {
+        let ft = ent
+            .file_type()
+            .map_err(|e| format!(
+                "Could not read {}: {}", ent.path().display(), e))?;
+        Ok(DirEntry {
+            path: ent.path(),
+            ft,
+        })
+    }
+
+    /// Return the file name of this entry.
+    ///
+    /// If this entry has no file name (e.g., `/`), then the full path is
+    /// returned.
+    pub fn file_name(&self) -> &OsStr {
+        self.path.file_name().unwrap_or_else(|| self.path.as_os_str())
+    }
+
+    pub fn is_dir(&self) -> bool {
+        self.ft.is_dir()
+    }
+
+    pub fn is_file(&self) -> bool {
+        self.ft.is_file()
+    }
+
+    pub fn is_symlink(&self) -> bool {
+        self.ft.is_symlink()
+    }
+
+}
+
+
+/// The entry will always be a directory, and this should return True
+/// if we should also traverse children.
+fn should_traverse_dir(entry: &DirEntry) -> bool {
+    entry.path
+     .to_str()
+     .map(|n|
+         !n.ends_with("External/Ada_Web_Server/aws-dev")
+         && !n.ends_with("External/GNATCOLL/gnatcoll-dev")
+         && !n.ends_with("Examples/Elektron/Ema/Training")
+         && !n.ends_with("Packaging")
+         && !n.ends_with("Compiler")
+         && !n.ends_with(".dbc")
+         && !n.ends_with(".git")
+         && !n.ends_with("__pycache__")
+         && !n.ends_with("objects"))
+     .unwrap_or(false)
+}
+
+// pub fn findfile(root: &Path) -> dyn Iterator<Item=PathBuf> {
+//     WalkDir::new(root)
+//         .follow_links(false)
+//         .into_iter()
+//         .filter_entry(should_traverse)   // Omit some directories from path
+//         .filter_map(|e| e.ok())          // Ignore errors
+//         .map(|e| e.path())
+// }
+
 
 pub struct FileFind {
-    iter: Vec<std::fs::ReadDir>,
+    stack: Vec<std::fs::ReadDir>,
 }
 
 impl FileFind {
     /// Start searching for file in path, recursively
     pub fn new(path: &Path) -> FileFind {
-        let mut f = FileFind { iter: vec![] };
+        let mut f = FileFind { stack: vec![] };
         f.pushdir(path);
         f
     }
@@ -18,36 +91,13 @@ impl FileFind {
     /// and so on).
     fn pushdir(&mut self, path: &Path) {
         match std::fs::read_dir(path) {
-            Ok(iter) => self.iter.push(iter),
+            Ok(readdir) => self.stack.push(readdir),
             Err(err) => {
                 println!("Error reading directory {}: {}", path.display(), err);
             }
         }
     }
 
-    /// Whether the directory should be traversed
-    fn traverse_dir(&mut self, path: &Path) -> bool {
-        match path.as_os_str().to_str() {
-            None => false,
-            Some(n) => {
-                if n.ends_with("External/Ada_Web_Server/aws-dev")
-                    || n.ends_with("External/GNATCOLL/gnatcoll-dev")
-                    || n.ends_with("Examples/Elektron/Ema/Training")
-                    || n.ends_with("Packaging")
-                    || n.ends_with("Compiler")
-                    || n.ends_with(".dbc")
-                    || n.ends_with(".git")
-                    || n.ends_with("objects")
-                {
-                    false
-                } else if let Ok(meta) = std::fs::symlink_metadata(path) {
-                    !meta.is_symlink()
-                } else {
-                    false
-                }
-            }
-        }
-    }
 }
 
 impl Iterator for FileFind {
@@ -55,32 +105,27 @@ impl Iterator for FileFind {
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            match self.iter.last_mut()?.next() {
+            match self.stack.last_mut()?.next() {
                 None => {
-                    self.iter.pop(); //  Nothing else to read in child directory
+                    self.stack.pop(); //  Nothing else to read in directory
                 }
                 Some(Ok(e)) => {
-                    let path = e.path();
-                    match e.file_type() {
-                        Err(err) => println!(
-                            "Error reading {}: {}",
-                            path.display(),
-                            err
-                        ),
-                        Ok(ft) => {
-                            if ft.is_dir() {
-                                if self.traverse_dir(&path) {
-                                    self.pushdir(&path);
+                    match DirEntry::from_entry(&e) {
+                        Err(err) => println!("{}", err),
+                        Ok(e) => {
+                            if e.is_dir() {
+                                if !e.is_symlink() && should_traverse_dir(&e) {
+                                    self.pushdir(&e.path);
                                 }
-                            } else if ft.is_file() {
+                            } else if e.is_file() {
                                 if let Some("gpr") =
-                                    path.extension().and_then(OsStr::to_str)
+                                    e.path.extension().and_then(OsStr::to_str)
                                 {
-                                    return Some(path);
+                                    return Some(e.path);
                                 }
                             }
                         }
-                    }
+                    };
                 }
                 Some(Err(err)) => {
                     println!("Error {}", err);
